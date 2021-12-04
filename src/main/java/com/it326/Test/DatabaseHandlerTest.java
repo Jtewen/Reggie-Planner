c6a45ed6f22a911e728222a13346e6a76c923747
josephgearlds <43246427+josephgearlds@users.noreply.github.com>
2021-12-04 15:16:44 -0600

    Trying to add Junit

---
+++ src/main/java/com/it326/Test/DatabaseHandlerTest.java
@@ -0,0 +1,14 @@
+package com.it326.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.*;
+
+import org.junit.Test;
+
+
+public class DatabaseHandlerTest {
+    @Test
+    public void testAddCourse()
+    {
+
+    }
+}

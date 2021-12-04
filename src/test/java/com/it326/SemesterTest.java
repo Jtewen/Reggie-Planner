d96aa7888adf0ce7abe290fdb4c5adc3e1faa26a
josephgearlds <43246427+josephgearlds@users.noreply.github.com>
2021-12-04 16:14:29 -0600

    Set Up Junit

---
+++ src/test/java/com/it326/SemesterTest.java
@@ -0,0 +1,18 @@
+package com.it326;
+import static org.junit.Assert.assertEquals;
+
+
+import org.junit.Test;
+
+
+public class SemesterTest {
+    @Test
+    public void testAddCourse()
+    {
+        Account acc = new Account("Joseph", "test");
+        Schedule sch = acc.getManager().getSchedule();
+        Semester sem = sch.addSemester();
+        Course c = new Course("IT", 326, "Principles of Software Engineering", 3, "IT 261", "This is a Decriptions");
+        assertEquals(true, sem.addCourse(c));
+    }
+}

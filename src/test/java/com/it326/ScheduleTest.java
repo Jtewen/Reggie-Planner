a51d47d2f26b0355f57e5300700db766dc5c8a99
bhayden51 <bradley8451@hotmail.com>
2021-12-04 19:40:41 -0600

    unit tests

--- src/test/java/com/it326/ScheduleTest.java
+++ src/test/java/com/it326/ScheduleTest.java
@@ -1,5 +1,63 @@
 package com.it326;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 public class ScheduleTest {
-    
+    @Test()
+    public void testScheduleConEmpty()
+    {
+        Schedule t = new Schedule();
+    }
+
+    @Test()
+    public void testScheduleCon()
+    {
+        Schedule t = new Schedule("TestName");
+        assertEquals("TestName", t.getName());
+    }
+
+    @Test()
+    public void testAddSemesterEmpty()
+    {
+        Schedule t = new Schedule();
+        Semester testSem = t.addSemester();
+    }
+
+    @Test()
+    public void testAddSemester()
+    {
+        Schedule t = new Schedule();
+        Semester testSem = t.addSemester("Fall", 2021);
+        assertEquals("Fall", testSem.getSeason());
+        assertEquals(2021, testSem.getYear());
+    }
+
+    @Test()
+    public void testAddSemester2()
+    {
+        Schedule t = new Schedule();
+        Semester testSem = new Semester("Fall", 2021);
+        t.addSemester(testSem);
+    }
+
+    @Test()
+    public void testRemoveSemester()
+    {
+        Schedule t = new Schedule();
+        Semester testSem = t.addSemester("Fall", 2021);
+        t.removeSemester(testSem);
+        assertEquals(0, t.getSemesters().size());
+    }
+
+    @Test()
+    public void testAddExternalCreds()
+    {
+        Schedule t = new Schedule();
+        t.addExternalCreds(5);
+        assertEquals(5, t.getExternalCreds());
+    }
+
+
+
+
 }
